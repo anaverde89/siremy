fbe2656455ce5ab9f74b7701f53a2eb4767b6ed9
Ana Verde <anaverde89@gmail.com>
2021-07-04 20:32:24 -0400

    Solucion de merge

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   presets: ["@vue/cli-plugin-babel/preset"],
 };
-=======
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
->>>>>>> aa7c83d87d02eb3466fdb6332b52807781056460
--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-  plugins: [require('tailwindcss')('tailwind.js'), require('autoprefixer')()]
-}
->>>>>>> aa7c83d87d02eb3466fdb6332b52807781056460
