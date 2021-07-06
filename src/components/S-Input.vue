02d3378adcbb5c79e393411fff45a869f5e3b9fc
Ana Verde <anaverde89@gmail.com>
2021-07-06 15:59:56 -0400

    Creando primeros componentes

---
+++ src/components/S-Input.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="
+      wrap-input
+      flex flex-wrap
+      items-end
+      w-full
+      relative
+      border border-solid
+    "
+  >
+    <slot></slot>
+    <span
+      class="
+        focus-input
+        absolute
+        block
+        -top-px
+        -left-px
+        pointer-events-none
+        border border-solid
+        opacity-0
+        invisible
+      "
+    ></span>
+    <span
+      class="
+        label-input
+        font-montserrat-reg
+        leading-1.2
+        block
+        absolute
+        pointer-events-none
+        w-full
+        left-0
+        text-left
+      "
+      >{{ label }}</span
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SInput',
+  props: {
+    label: String,
+  },
+}
+</script>
+
+<style scoped>
+@import '../assets/css/styles-1.css';
+</style>
