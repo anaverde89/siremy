fca4da529037d863bf1a6d3910259ee35a986272
Ana Verde <anaverde89@gmail.com>
2021-07-28 15:54:58 -0400

    Header en pagina Dashboard

---
+++ src/components/dashboard/BtnHamburger.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button
+      type="button"
+      :class="['hamburger', classObject]"
+      :v-else="dataClass"
+      @click="$emit('clickClose')"
+    >
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BtnHamburger',
+  props: {
+    classObject: {
+      type: Array,
+      default: () => [],
+    },
+    dataClass: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
---
+++ src/components/dashboard/TxtSearch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="['search-wrapper', { active: isActive }]">
+    <div class="input-holder">
+      <input
+        type="text"
+        class="search-input"
+        :placeholder="phValue"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <button class="search-icon" @click="$emit('clickSearch')">
+        <span></span>
+      </button>
+    </div>
+    <button class="close" @click="$emit('closeSearch')"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TxtSearch',
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    phValue: {
+      type: String,
+      default: '',
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
