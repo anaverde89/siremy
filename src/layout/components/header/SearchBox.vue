a60097ed8de0a6f0d368ccc4ec17412aec1867f9
Ana Verde <anaverde89@gmail.com>
2021-08-24 10:07:32 -0400

    Base de datos de prueba

--- src/layout/components/header/SearchBox.vue
+++ src/layout/components/header/SearchBox.vue
@@ -2,12 +2,20 @@
   <div>
     <div class="search-wrapper" v-bind:class="{ active: searchOpen }">
       <div class="input-holder">
-        <input type="text" class="search-input" placeholder="Type to search" />
-        <button class="search-icon" v-on:click="searchOpen = !searchOpen">
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Buscar..."
+          v-model="textSearch"
+        />
+        <button
+          class="search-icon"
+          v-on:click="searchOpen ? search() : toggleOpen()"
+        >
           <span />
         </button>
       </div>
-      <button class="close" v-on:click="searchOpen = !searchOpen" />
+      <button class="close" v-on:click="toggleOpen()" />
     </div>
   </div>
 </template>
@@ -17,7 +25,17 @@ export default {
   data() {
     return {
       searchOpen: false,
+      textSearch: '',
     }
+  },
+  methods: {
+    toggleOpen() {
+      this.searchOpen = !this.searchOpen
+      if (!this.searchOpen) {
+        this.textSearch = ''
+      }
+    },
+    search() {},
   },
 }
 </script>
